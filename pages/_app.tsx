bc644a5b206ed7fc3758d52ea7f2472146ed2ca0
cococov <juank.yea@hotmail.com>
2021-10-31 02:51:00 -0300

    feat: add fonawesome icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/globals.css';
+import 'styles/baseStyles.css';
+import 'styles/tailwindUtilityClasses.css';
+import type { AppProps } from 'next/app';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faJava,
+  faReact,
+  faPython,
+  faJsSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import { faCopyright } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faJava, faReact, faPython, faJsSquare, faCopyright);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
