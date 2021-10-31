335a355f3881bb13c7bc0b92e6bfcdf65e9c57fc
cococov <juank.yea@hotmail.com>
2021-10-31 02:50:26 -0300

    feat: config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,28 @@ module.exports = {
     },
     colors: {
       light: '#333842',
-      main: '#282c34',
+      main: '#282C34',
+      mainLight: '#444953',
       dark: '#070709',
       text: '#E8F3F8',
+      darkText: '#C6D1D6',
       backgroundColor: '#1d2026'
+    },
+    fontFamily: {
+      sans: ['Helvetica', 'Arial', 'sans-serif'],
+      jetbrains: ['JetBrains Mono', 'monospace'],
+      roboto: ['Roboto', 'sans-serif'],
+    },
+    boxShadow: {
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      '3xl': '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none',
     }
   },
   variants: {
